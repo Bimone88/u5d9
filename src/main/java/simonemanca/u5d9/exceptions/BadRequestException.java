500d20caaf476287f4b91f61d33cd42d886e4573
Simone Manca <simone.manca.98@gmail.com>
2024-04-11 16:44:49 +0200

    reset

--- src/main/java/simonemanca/u5d9/exceptions/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package gioelefriggiau5d8hw.exceptions;
-
-public class BadRequestException extends RuntimeException {
-
-    public BadRequestException(String message) {
-        super(message);
-    }
-
-}
